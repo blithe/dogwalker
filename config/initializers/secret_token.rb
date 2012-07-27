c041cc771e6e68356fc36398663b59520d2ef6ee
Blithe Rocher <blitherocher@gmail.com>
2012-07-27 17:41:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogwalker::Application.config.secret_token = '050638265dbcb3cbbe15351d1844464571ed4773987d523e1b8ae974003e2eaa644090ef3ee83b922e8d9fb179ce0df7cace015e16bfb8237fc0573f7650eee5'
