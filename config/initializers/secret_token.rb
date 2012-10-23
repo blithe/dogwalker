0e42f2373439ce9fdb6b6c6e533cdd42ea73e035
Blithe Rocher <blitherocher@gmail.com>
2012-10-22 20:57:56 -0400

    Update Application name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '176592f320c095f811a4869ab89d051d3b02514454d78e054d976ecfe11a9fc2a4a95e753f6554ea613320b0912e5a7243ac00bad1f217dff428faafc946c4df'
+DogwalkerApp::Application.config.secret_token = '176592f320c095f811a4869ab89d051d3b02514454d78e054d976ecfe11a9fc2a4a95e753f6554ea613320b0912e5a7243ac00bad1f217dff428faafc946c4df'
