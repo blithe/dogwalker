e01a13f4a97a9a1393fb5916092ec60d1f1284b4
Blithe Rocher <blitherocher@gmail.com>
2012-11-10 13:02:56 -0500

    Add dog creation interface

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ DogwalkerApp::Application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-  resources :dogs, only: [:create, :destroy]
+  resources :dogs, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
