eb22a3f443fe852006d1a482d72ca3740da090d8
Blithe Rocher <blitherocher@gmail.com>
2013-02-11 22:13:47 -0500

    add marker to map

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-  var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+  	var mapOptions = {
+    	center: new google.maps.LatLng(-34.397, 150.644),
+    	zoom: 8,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    if (document.getElementById("map_canvas")) {
-	    var map = new google.maps.Map(document.getElementById("map_canvas"),
-	        mapOptions);
-	}
+    
+	if (document.getElementById("map_canvas")) {
+		var map = new google.maps.Map(document.getElementById("map_canvas"),
+	    	mapOptions);
+	};
+
+
+    var marker = new google.maps.Marker({
+	    position: map.getCenter(),
+	    map: map,
+	    title: 'Click to zoom'
+	});
 });
