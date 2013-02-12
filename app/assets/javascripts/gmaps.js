16cece3265359eb25f4eb4c43d3bb2ee95ec6027
Blithe Rocher <blitherocher@gmail.com>
2013-02-11 21:52:02 -0500

    add map to dogs page

---
+++ app/assets/javascripts/gmaps.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    if (document.getElementById("map_canvas")) {
+	    var map = new google.maps.Map(document.getElementById("map_canvas"),
+	        mapOptions);
+	}
+});
